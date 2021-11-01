a579fc06eaef1589666d2fc7b12a93b5077202a4
Kavin <quantakavin@Quantas-MacBook-Pro.local>
2021-11-01 14:53:39 +0800

    add comments for creating tables

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -5,17 +5,93 @@ const connectionString = "postgres://jquycoxe:***@fanny.db.elephantsql.com/jquyc
 
 const pool = new Pool({
   connectionString,
+  max: 5,
 })
 
 
-/*
-const pool = new Pool({
-    user: 'dbuser',
-    host: 'database.server.com',
-    database: 'mydb',
-    password: 'secretpassword',
-    port: 3211,
-  })
-*/
 
   module.exports = pool;
+
+  /*
+
+  DROP TABLE IF EXISTS users; 
+  CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  email VARCHAR UNIQUE NOT NULL,
+  password VARCHAR NOT NULL,
+  privacy BOOLEAN NOT NULL DEFAULT false,
+  active BOOLEAN NOT NULL DEFAULT false
+  )
+
+DROP TABLE IF EXISTS profile; 
+CREATE TABLE profile (
+id SERIAL PRIMARY KEY,
+user_id INT NOT NULL,
+bio VARCHAR,
+profilepic bytea,
+coverpic bytea,
+CONSTRAINT fk_user
+      FOREIGN KEY(user_id) 
+	  REFERENCES users(id)
+)
+
+DROP TABLE IF EXISTS page; 
+CREATE TABLE page (
+id SERIAL PRIMARY KEY,
+user_id INT NOT NULL,
+description VARCHAR,
+url VARCHAR,
+pagepic bytea,
+coverpic bytea,
+CONSTRAINT fk_user
+      FOREIGN KEY(user_id) 
+	  REFERENCES users(id)
+)
+
+DROP TABLE IF EXISTS post; 
+CREATE TABLE post (
+id SERIAL PRIMARY KEY,
+user_id INT NOT NULL REFERENCES users(id),
+page_id INT REFERENCES page(id),
+content VARCHAR,
+file bytea,
+type VARCHAR NOT NULL
+)
+
+DROP TABLE IF EXISTS comment; 
+CREATE TABLE comment (
+id SERIAL PRIMARY KEY,
+user_id INT NOT NULL REFERENCES users(id),
+post_id INT NOT NULL REFERENCES post(id),
+content VARCHAR NOT NULL,
+)
+
+DROP TABLE IF EXISTS likes;
+CREATE TABLE likes (
+user_id INT NOT NULL REFERENCES users(id),
+post_id INT NOT NULL REFERENCES post(id),
+PRIMARY KEY(user_id, post_id)
+)
+
+DROP TABLE IF EXISTS friendship;
+CREATE TABLE friendship (
+user_id INT NOT NULL REFERENCES users(id),
+friend_id INT NOT NULL REFERENCES users(id),
+confirmed BOOLEAN DEFAULT false,
+PRIMARY KEY(user_id, friend_id)
+)
+
+DROP TABLE IF EXISTS conversation;
+CREATE TABLE conversation (
+user_id INT NOT NULL REFERENCES users(id),
+friend_id INT NOT NULL REFERENCES users(id),
+PRIMARY KEY(user_id, friend_id)
+)
+
+
+
+
+
+
+  */
