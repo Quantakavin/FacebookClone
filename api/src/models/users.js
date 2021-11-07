2b473e265dfa1c3f4d078815250fa6ad5b7b9399
BrennanOwYong <brennanowyong@gmail.com>
2021-11-07 22:22:45 +0800

    made the "get profile by id" feature
    if users getting their own profile, all's ok. if not, then profile wont show if it is set to be private

--- api/src/models/users.js
+++ api/src/models/users.js
@@ -1,4 +1,4 @@
-var jwt=require('jsonwebtoken');
+var jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const connection = require('../config/database');
 
@@ -6,12 +6,22 @@ module.exports.insert = (name, email, password, callback) => {
     const insertUserQuery = `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id;`;
     const values = [name, email, password];
     connection.query(insertUserQuery, values)
-    .then(returnid => {
-        callback(returnid, null)
-    })
-    .catch(err => callback(null, err))
+        .then(returnid => {
+            callback(returnid, null)
+        })
+        .catch(err => callback(null, err))
 }
 
 module.exports.login = () => {
-    
+
+}
+
+module.exports.getProfile = (getterID, gottenID, callback) => {
+    const sql = "SELECT * FROM users where id = $1 "
+    const privacyCondition = getterID == gottenID ? ";" : "AND privacy = false;" ;
+    connection.query(gottenID, sql+privacyCondition)
+        .then(returnid => {
+            callback(returnid, null)
+        })
+        .catch(err => callback(null, err))
 }
