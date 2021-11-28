b200e84d92876e1146e1196bd8030f9ca1a8be50
BrennanOwYong <brennanowyong@gmail.com>
2021-11-28 16:12:00 +0800

    profile photo upload 90% done, bio update not done

---
+++ api/src/models/comment.js
@@ -0,0 +1,38 @@
+const connection = require('../config/database');
+
+module.exports.getComments = async (callback) => {
+
+}
+
+module.exports.update = async (userid, commentID, newComment, callback) => {
+    var updateCommentQuery = `update comment set content = $1, editdate = now() where user_id = $2 AND id = $3`
+    connection.query(updateCommentQuery, [newComment, userid, commentID])
+        .then(result => {
+            console.log(result)
+            if (result.rowCount == 0) {
+                return callback(null, "update failed. either commentID or userid was wrong")
+            }
+            callback(result, null)
+        })
+        .catch(err => {
+            console.log(err)
+            callback(null, err)
+        })
+}
+
+module.exports.createComment = async (userid, postid, commentText,callback) => {
+    var makeComment = `insert into comment (user_id, post_id, content, date) values ( $1, $2, $3, now() )`
+    connection.query(makeComment, [userid, postid, commentText,])
+        .then(result => {
+            console.log(result)
+            callback(result, null)
+        })
+        .catch(err => {
+            console.log(err)
+            callback(null, err)
+        })
+}
+
+module.exports.deleteComment = async (callback) => {
+
+}
