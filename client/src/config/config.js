f7fcf3b35af269f879c33204634b2acf6d81308e
Kavin <quantakavin@Quantas-MacBook-Pro.local>
2021-11-28 09:43:22 +0800

    fix environment error

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,9 @@
-let baseURL = process.env.REACT_APP_DEVELOPMENT_URL;
-
-if (process.env.NODE_ENV == 'production') {
+var baseURL;
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    baseURL = process.env.REACT_APP_DEVELOPMENT_URL;
+} else {
     baseURL = process.env.REACT_APP_PRODUCTION_URL;
 }
-
 
 module.exports = {
     baseURL: baseURL
