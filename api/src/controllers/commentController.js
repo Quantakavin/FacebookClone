7193384404d2600b489ea9c76d842a7de0e90b08
Kavin <quantakavin@Quantas-MacBook-Pro.local>
2021-12-02 00:52:41 +0800

    add backend validation for comments

--- api/src/controllers/commentController.js
+++ api/src/controllers/commentController.js
@@ -40,6 +40,35 @@ module.exports.getComment = async (req,res) => {
 module.exports.updateComment = async (req, res) => {
     var id = req.params.id;
     var { userid, content} = req.body
+
+    try {
+        await comment.getById(id, (results, error) => {
+            if(error) {
+                console.log(error)
+                return res.status(500).json({error: "Cannot find comment"});
+            } else {
+                if (results[0].id == userid) {
+                    comment.update(userid, id, content, (results2, err) => {
+                        if (err) {
+                            console.log(err)
+                            return res.status(500).json({ err });
+                        } else {
+                            return res.status(200).json(results2)
+                        }
+                    })
+                } else {
+                    return res.status(402).json({"message":"You do not have access"});
+                }
+            }
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error: "Cannot find comment"});
+    }
+
+
+    /*
     try {
         comment.update(userid, id, content, (results, err) => {
             if (err) {
@@ -53,6 +82,7 @@ module.exports.updateComment = async (req, res) => {
         console.log(error)
         res.status(500).json({ message: "" })
     }
+    */
 }
 
 module.exports.createComment = async (req, res) => {
@@ -74,7 +104,35 @@ module.exports.createComment = async (req, res) => {
 
 module.exports.deleteComment = async (req, res) => {
     var commentid = req.params.id
+    var { userid } = req.body
     try {
+
+
+        await comment.getById(commentid, (results, error) => {
+            if(error) {
+                console.log(error)
+                return res.status(500).json({error: "Cannot find comment"});
+            } else {
+                if (results[0].id == userid) {
+                    comment.deleteComment(commentid, (results2, err) => {
+                        if (err) {
+                            console.log(err)
+                            return res.status(500).json({ message:"delete comment failed in backend" });
+                        } else {
+                            return res.status(200).json(results2)
+                        }
+                    })
+                } else {
+                    return res.status(402).json({"message":"You do not have access"});
+                }
+                return res.status(200).json(results[0]);
+            }
+        })
+
+
+
+
+/*
         comment.deleteComment(commentid, (results, err) => {
             if (err) {
                 console.log(err)
@@ -83,6 +141,7 @@ module.exports.deleteComment = async (req, res) => {
                 return res.status(204).json(results)
             }
         })
+        */
 
     } catch (error) {
         console.log(error)
