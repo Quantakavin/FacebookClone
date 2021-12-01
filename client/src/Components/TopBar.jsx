d33501b9f548836eca9769ab22b2400e028c946d
Kavin <quantakavin@Quantas-MacBook-Pro.local>
2021-12-02 01:58:08 +0800

    remove fixed top navbar

--- client/src/Components/TopBar.jsx
+++ client/src/Components/TopBar.jsx
@@ -14,7 +14,7 @@ const TopBar = () => {
   if (localStorage.getItem("token") == null) {
     return (
       <>
-        <Navbar fixed="top" variant="dark" expand="lg" style={{ backgroundColor: "#4267B2" }}>
+        <Navbar variant="dark" expand="lg" style={{ backgroundColor: "#4267B2" }}>
           <Container fluid>
             <Navbar.Brand className="brand" href="/home"><img
               alt=""
@@ -45,7 +45,7 @@ const TopBar = () => {
     return (
       <>
 
-        <Navbar fixed="top" variant="dark" expand="lg" style={{ backgroundColor: "#4267B2" }}>
+        <Navbar variant="dark" expand="lg" style={{ backgroundColor: "#4267B2" }}>
           <Container fluid>
             <Navbar.Brand className="brand" href="/userhome"><img
               alt=""
