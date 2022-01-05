c538ea6e12a6c8c86708852ad490d68cd977e26d
KannanMedhun <medhun.20@ichat.sp.edu.sg>
2022-01-05 12:01:30 +0800

    search.js simple search using LIKE

--- api/src/models/search.js
+++ api/src/models/search.js
@@ -4,3 +4,13 @@ const connection = require('../config/database');
 
 var validator = require('validator');
 
+module.exports.search =  async (name, callback) => {
+    const searchquery = 'SELECT * FROM users where name LIKE $1 '
+    connection.query(searchquery, ['%' + name + '%'])
+    .then(result => {
+        console.log(result.rows[0])
+        callback(result.rows[0], null)
+    })
+    .catch(err => callback(null, err))
+
+}
