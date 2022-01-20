80b8838ba1c246e9b686b866dc8f649ab4920557
Yiping <syiping.20@ichat.sp.edu.sg>
2022-01-20 13:11:33 +0800

    500 error page

---
+++ client/src/Pages/500.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const InternalError = () => {
+    return (
+        <div id="wrapper">
+            <img src="https://i.imgur.com/qIufhof.png" />
+            <div id="info">
+                <h3>Internal server error</h3>
+            </div>
+        </div >
+    )
+}
+
+export default InternalError
