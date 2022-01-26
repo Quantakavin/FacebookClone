ecf1f53eeba53e89a1219a83fe335b1da2ba38a3
Yiping <syiping.20@ichat.sp.edu.sg>
2022-01-27 00:55:13 +0800

    format

--- client/src/Pages/Notifications.jsx
+++ client/src/Pages/Notifications.jsx
@@ -4,7 +4,6 @@ import axios from 'axios';
 import config from '../config/config';
 import { Container } from 'react-bootstrap';
 
-
 const Notifications = (props) => {
     const [notification, setNotification] = useState([]);
 
@@ -12,41 +11,39 @@ const Notifications = (props) => {
         getNotification()
     })
 
-
     const getNotification = () => {
-        axios.get(`${config.baseURL}/notification?receiver_id=${localStorage.getItem("user_id")}`,{
+        axios.get(`${config.baseURL}/notification?receiver_id=${localStorage.getItem("user_id")}`, {
             headers: {
-                'Authorization': `Bearer ${localStorage.getItem('token')}` 
-              }
-          })
-          .then(response => {
-            setNotification(response.data)
-          })
-          .catch(error => {
-            console.log(error);
-            
-          })
-    
-      }
-      
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+            .then(response => {
+                setNotification(response.data)
+            })
+            .catch(error => {
+                console.log(error);
+
+            })
+
+    }
 
     return (
         <>
-        <header>
-            <TopBar />
-        </header>
-        <div id="wrapper">
-            <div id="info">
-                <h2 style={{marginTop: 50, marginBottom: 30}}>Notifications</h2>
-                {notification.map(notifications=>
-                            <div>
-                               <p>{notifications.name} {notifications.notification_message}</p> 
-                            </div>
+            <header>
+                <TopBar />
+            </header>
+            <div id="wrapper">
+                <div id="info">
+                    <h2 style={{ marginTop: 50, marginBottom: 30 }}>Notifications</h2>
+                    {notification.map(notifications =>
+                        <div>
+                            <p>{notifications.name} {notifications.notification_message}</p>
+                        </div>
                     )}
-                 
-            </div>
-            
-        </div >
+
+                </div>
+
+            </div >
         </>
     )
 }
