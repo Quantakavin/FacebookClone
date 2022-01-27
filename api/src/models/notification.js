e409ecfda5feeb5b4b39d212b5cf917e4e3e5aa2
Yiping <syiping.20@ichat.sp.edu.sg>
2022-01-27 14:36:40 +0800

    yes

--- api/src/models/notification.js
+++ api/src/models/notification.js
@@ -35,21 +35,6 @@ module.exports.friendNotification = async (receiver_id, notification_id, userid)
     })
 }
 
-// module.exports.getNotifications = async (receiver_id) => {
-//     const sql = `SELECT notification.*, users.name, notificationmessage.notification_message FROM notification INNER JOIN users ON users.id::varchar(255) = notification.DATA ->> 'userid' INNER JOIN notificationmessage ON notification.notification_id = notificationmessage.id WHERE (notification.receiver_id = $1)`
-//     return new Promise((resolve, reject) => {
-//         connection
-//             .query(sql, [receiver_id])
-//             .then((result) => {
-//                 resolve(result)
-//             })
-//             .catch((err) => {
-//                 console.log(err)
-//                 reject(err)
-//             })
-//     })
-// }
-
 
 module.exports.getNotifications = async (receiver_id) => {
     const sql = `SELECT notification.*,notification.data ->> 'userid' AS userid, users.name, notificationmessage.notification_message FROM notification INNER JOIN users ON users.id = CAST((notification.data ->> 'postid') AS INTEGER) INNER JOIN notificationmessage ON notification.notification_id = notificationmessage.id WHERE (notification.receiver_id = $1) order by time desc`
