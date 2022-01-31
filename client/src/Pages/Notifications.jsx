3ffa4767c01355f508b95b52febf686033ae785b
Minnnnno <mervynphua9@gmail.com>
2022-01-31 14:09:00 +0800

    added some things

--- client/src/Pages/Notifications.jsx
+++ client/src/Pages/Notifications.jsx
@@ -1,54 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import TopBar from '../Components/TopBar';
-import axios from 'axios';
-import config from '../config/config';
-import { Container } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import TopBar from "../Components/TopBar";
+import axios from "axios";
+import config from "../config/config";
+import { Container } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 
 const Notifications = () => {
-    const [notification, setNotification] = useState([]);
-    const history = useHistory();
+  const [notification, setNotification] = useState([]);
+  const history = useHistory();
 
-    useEffect(() => {
-        getNotification()
-    })
+  useEffect(() => {
+    getNotification();
+  });
 
-    const getNotification = () => {
-        axios.get(`${config.baseURL}/notification?receiver_id=${localStorage.getItem("user_id")}`, {
-            headers: {
-                'Authorization': `Bearer ${localStorage.getItem('token')}`
-            }
-        })
-            .then(response => {
-                setNotification(response.data)
-            })
-            .catch(error => {
-                console.log(error);
+  const getNotification = () => {
+    axios
+      .get(
+        `${config.baseURL}/notification?receiver_id=${localStorage.getItem(
+          "user_id"
+        )}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      )
+      .then((response) => {
+        setNotification(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-            })
+  return (
+    <>
+      <header>
+        <TopBar />
+      </header>
+      <div id="wrapper">
+        <div id="info">
+          <h2 style={{ marginTop: 50, marginBottom: 30 }}>Notifications</h2>
+          {notification.map((n) => {
+            console.log(n);
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
-    }
-
-    return (
-        <>
-            <header>
-                <TopBar/>
-            </header>
-            <div id="wrapper">
-                <div id="info">
-                    <h2 style={{ marginTop: 50, marginBottom: 30 }}>Notifications</h2>
-                    {notification.map(notifications =>
-                        <div>
-                            <span onClick={() => { history.push(`./profile/${notifications.userid}`) }} > {notifications.name}</span>
-                            <span onClick={() => { history.push(`./post/${notifications.postid}`) }}>{notifications.notification_message}</span>
-                        </div>
-                    )}
-
-                </div>
-
-            </div >
-        </>
-    )
-}
-
-export default Notifications
+export default Notifications;
