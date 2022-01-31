3b8f3f8a8ca0d62a29e1ed7f1645f1afebc3eaf0
Kavin <quantakavin@Quantas-MacBook-Pro.local>
2022-01-31 09:21:58 +0800

    add link to friends list

--- client/src/Pages/ViewProfile.jsx
+++ client/src/Pages/ViewProfile.jsx
@@ -453,7 +453,7 @@ const Profile = ({ match }) => {
                     {friend.name}
                   </p>
                   <div style={{ justifyContent: "flex-end" }}>
-                    <Button className="btn btn-primary">View Profile</Button>
+                    <Button className="btn btn-primary" onClick = {() => history.push(`/profile/${friend.id}`)}>View Profile</Button>
                   </div>
                 </div>
               ))}
